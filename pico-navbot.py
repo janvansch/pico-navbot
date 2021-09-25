f873e9b311f23c2138075be1c1fe745cc5fe75a1
janvansch <jvs@nimbledesign.co.za>
2021-09-25 11:58:13 +0200

    4th

--- pico-navbot.py
+++ pico-navbot.py
@@ -2,46 +2,56 @@
 from machine import Pin, PWM, Timer
 import utime
 
-#analogvalue = machine.ADC(28)
-
 # ------------------------
 #  Initialisation Section
 # ------------------------
 
 # Initialise some global variables
 distance = 0
-left_rear_encoder_count = 0
+front_left_encoder_count = 0
+front_right_encoder_count = 0
+rear_left_encoder_count = 0
+rear_right_encoder_count = 0
+
+# Onboard LED object
+led = Pin(25, Pin.OUT)
 
 # Define timer object
 timer = Timer()
 
 # Setup PWM object for servo
-pwm = PWM(Pin(0))
+pwm = PWM(Pin(16))
 pwm.freq(50)
 
-# Onboard LED object
-led = Pin(25, Pin.OUT)
+# UART communication with Bluetooth module
+# Connect GP0 (UART0 Tx) to Rx of HC-05/06 Bluetooth module (brown)
+# Connect GP1 (UART0 Rx) to Tx of HC-05/06 Bluetooth module (white)
+# Create UART object connect to UART channel 0 at a baud rate of 9600
+uart = UART(0, 9600)
 
 # IR sensor interface
-ir_front_centre = Pin(2, Pin.IN)
-ir_left_front = Pin(4, Pin.IN)
-ir_right_front = Pin(5, Pin.IN)
+ir_front_left = Pin(5, Pin.IN) # orange
+ir_front_centre = Pin(4, Pin.IN) # yellow
+ir_front_right = Pin(3, Pin.IN) # green
+ir_rear_centre = Pin(2, Pin.IN) # blue
+# ir_mid_left = Pin(7, Pin.IN) # light grey
+# ir_mid_right = Pin(6, Pin.IN) # purple
 
 # Sonic sensor interface 
 trigger = Pin(14, Pin.OUT)
 echo = Pin(15, Pin.IN)
 
 # Motor speed encoder interface
-#encoder_front_right = Pin(10, Pin.IN)
-#encoder_front_left = Pin(11, Pin.IN)
-#encoder_rear_right = Pin(12, Pin.IN)
-encoder_rear_left = Pin(13, Pin.IN)
+# encoder_front_left = Pin(10, Pin.IN) # blue
+encoder_front_right = Pin(11, Pin.IN) # brown
+encoder_rear_left = Pin(12, Pin.IN) # white
+# encoder_rear_right = Pin(13, Pin.IN) # purple
 
 # Motor control interface 
-motor_1a = Pin(6, Pin.OUT) # L298N IN4 orange
-motor_1b = Pin(7, Pin.OUT) # L298N IN3 yellow
-motor_2a = Pin(8, Pin.OUT) # L298N IN2 green
-motor_2b = Pin(9, Pin.OUT) # L298N IN1 blue
+motor_1a = Pin(18, Pin.OUT) # L298N IN4 orange
+motor_1b = Pin(19, Pin.OUT) # L298N IN3 yellow
+motor_2a = Pin(20, Pin.OUT) # L298N IN2 green
+motor_2b = Pin(21, Pin.OUT) # L298N IN1 blue
 # Remember to connect Pico GND to L298N GND
 
 # ---------------------------------------
@@ -50,12 +60,36 @@ motor_2b = Pin(9, Pin.OUT) # L298N IN1 blue
 #
 # Define speed interrupt handlers
 #
-def left_rear_encoder_counter(pin):
-    global left_rear_encoder_count
-    left_rear_encoder_count += 1
+
+def front_left_encoder_counter(pin):
+    global front_left_encoder_count
+    front_left_encoder_count += 1
+
+def front_right_encoder_counter(pin):
+    global front_right_encoder_count
+    front_right_encoder_count += 1
+
+def rear_left_encoder_counter(pin):
+    global rear_left_encoder_count
+    rear_left_encoder_count += 1
+
+def rear_right_encoder_counter(pin):
+    global rear_right_encoder_count
+    rear_right_encoder_count += 1
+
 #
 # Define obstical interrupt handlers
 #
+
+def obstical_front_left(pin):
+    if (pin.value() == 0):
+        led.on()
+        print("*** Obsticale - Front Left")
+        stop()
+        setServoCycle()
+    else:
+        led.off()
+        
 def obstical_front_centre(pin):
     if (pin.value() == 0):
         led.on()
@@ -65,16 +99,7 @@ def obstical_front_centre(pin):
     else:
         led.off()
 
-def obstical_left_front(pin):
-    if (pin.value() == 0):
-        led.on()
-        print("*** Obsticale - Front Left")
-        stop()
-        setServoCycle()
-    else:
-        led.off()
-
-def obstical_right_front(pin):
+def obstical_front_right(pin):
     if (pin.value() == 0):
         led.on()
         print("*** Obsticale - Front Right")
@@ -83,9 +108,37 @@ def obstical_right_front(pin):
     else:
         led.off()
 
+def obstical_rear_centre(pin):
+    if (pin.value() == 0):
+        led.on()
+        print("*** Obsticale - Rear Centre")
+        stop()
+        setServoCycle()
+    else:
+        led.off()
+
+def obstical_mid_left(pin):
+    if (pin.value() == 0):
+        led.on()
+        print("*** Obsticale - Mid left")
+        stop()
+        setServoCycle()
+    else:
+        led.off()
+
+def obstical_mid_right(pin):
+    if (pin.value() == 0):
+        led.on()
+        print("*** Obsticale - Mid Right")
+        stop()
+        setServoCycle()
+    else:
+        led.off()
+
 # -------------------------
 #  Servo Control Utilities
 # -------------------------
+
 def setServoCycle():
     for position in range (800, 8000, 50):
         pwm.duty_u16(position)
@@ -98,6 +151,7 @@ def setServoCycle():
 # -------------------------   
 #  Motor Control Utilities
 # -------------------------
+
 def stop():
     print("### Full Stop")
     motor_1a.low()
@@ -142,6 +196,7 @@ def get_distance():
     #Pause for two milliseconds to ensure the previous setting has completed 
     utime.sleep_us(2) 
     trigger.high()
+    
     # After pulling the high potential,
     # wait for 5 milliseconds,
     # and immediately set to Low
@@ -157,15 +212,15 @@ def get_distance():
     # and then record the time    
     while echo.value() == 1:         
         signalon = utime.ticks_us()
-        #Calculate the time difference between sending and receiving
-    
+        
+    #Calculate the time difference between sending and receiving
     duration = signalon - signaloff
 
     #Sonic travel time x speed of sound
     # (343.2 m/s , Which is 0.0343 cm per microsecond),
     # and the back-and-forth distance is divided by 2 
     distance = (duration * 0.0343) / 2            
-    # print('The distance is： ', distance, 'cm')
+    
     return distance
 
 def sonic_sense(timer):
@@ -179,13 +234,13 @@ def sonic_sense(timer):
         timer.deinit()
         led.on()
         print('Obstical within 5cm')
-        utime.sleep(2)
+        utime.sleep(0.5)
         led.off()
         while distance < 5:
             led.on()
-            utime.sleep(1)
+            utime.sleep(0.5)
             led.off()
-            utime.sleep(1)
+            utime.sleep(0.5)
             distance = get_distance()
         #start timer again
         timer.init(freq=1, mode=Timer.PERIODIC, callback=sonic_sense)
@@ -193,9 +248,18 @@ def sonic_sense(timer):
 # ------------------------
 #  Move Control Utilities
 # ------------------------
-def reset_left_rear_encoder_counter():
-    global left_rear_encoder_count
-    left_rear_encoder_count = 0
+def reset_front_left_encoder_counter():
+    global front_left_encoder_count
+    front_left_encoder_count = 0
+def reset_front_right_encoder_counter():
+    global front_right_encoder_count
+    front_right_encoder_count = 0
+def reset_rear_left_encoder_counter():
+    global rear_left_encoder_count
+    rear_left_encoder_count = 0
+def reset_rear_right_encoder_counter():
+    global rear_right_encoder_count
+    rear_right_encoder_count = 0
     
 def calc_clicks(leg_distance):
    # wheel diameter in mm
@@ -209,21 +273,23 @@ def calc_clicks(leg_distance):
 
 def move_forward(forward_distance):
     #global left_rear_encoder_count
-    reset_left_rear_encoder_counter()
+    reset_rear_left_encoder_counter()
     # Why does this work here but not at the end?????
+    reset_front_right_encoder_counter()
     
     clicks = calc_clicks(forward_distance)
     # Move forward while counter less than clicks
     print(">>> FORWARD >>>")
     print("--- Heading: 000, Distance: ", forward_distance)
-    print("--- Clicks: ", clicks, "Encoder Count: ", left_rear_encoder_count)
+    print("--- Clicks: ", clicks, "Encoder Count: ", rear_left_encoder_count)
     #forward()
-    while left_rear_encoder_count < clicks:
+    while rear_left_encoder_count < clicks:
         forward()
         pass    
     # Stop wheel when counter = clicks
     stop()
-    print("==>Encoder Count: ", left_rear_encoder_count)
+    print("==>Front Right Encoder Count: ", front_right_encoder_count)
+    print("==>Rear Left Encoder Count: ", rear_left_encoder_count)
 
 # -------------------------
 #  Main Navigation Control
@@ -240,32 +306,29 @@ def main():
     # Main loop
     run = True
     test_cycles = 2
-    test_time = 2
+    run_time = 2
+    stop_time = .5
     count = 1
     while run:
-        #reading = analogvalue.read_u16()
-        #pwm.duty_u16(int(reading/6))
         led.off()
-        # Forward Test (move_forward(distance in cm)
-        move_forward(150)
-        utime.sleep(test_time)
-        # stop()
-        utime.sleep(1)
+        # Forward Test (move_forward(distance in cm) and stop
+        move_forward(100)
+        utime.sleep(stop_time)
         # Reverse Test
         reverse()
-        utime.sleep(test_time)
+        utime.sleep(run_time)
         stop()
-        utime.sleep(1)
+        utime.sleep(stop_time)
         # Left Test
         left()
-        utime.sleep(test_time)
+        utime.sleep(run_time)
         stop()
-        utime.sleep(1)
+        utime.sleep(stop_time)
         # Right Test
         right()
-        utime.sleep(test_time)
+        utime.sleep(run_time)
         stop()
-        utime.sleep(1)
+        utime.sleep(stop_time)
         
         count = count + 1  
         if count > test_cycles:
@@ -280,13 +343,25 @@ def main():
 #
 # Setup IR obstical sensor interrupts
 #
+# - Front:
+ir_front_left.irq(handler=obstical_front_left, trigger=Pin.IRQ_FALLING|Pin.IRQ_RISING)
 ir_front_centre.irq(handler=obstical_front_centre, trigger=Pin.IRQ_FALLING|Pin.IRQ_RISING)
-ir_left_front.irq(handler=obstical_left_front, trigger=Pin.IRQ_FALLING|Pin.IRQ_RISING)
-ir_right_front.irq(handler=obstical_right_front, trigger=Pin.IRQ_FALLING|Pin.IRQ_RISING)
+ir_front_right.irq(handler=obstical_front_right, trigger=Pin.IRQ_FALLING|Pin.IRQ_RISING)
+# - Rear:
+ir_rear_centre.irq(handler=obstical_rear_centre, trigger=Pin.IRQ_FALLING|Pin.IRQ_RISING)
+# - Middle:
+# ir_mid_left.irq(handler=obstical_mid_left, trigger=Pin.IRQ_FALLING|Pin.IRQ_RISING)
+# ir_mid_right.irq(handler=obstical_mid_right, trigger=Pin.IRQ_FALLING|Pin.IRQ_RISING)
+
 #
 # Set speed encoder interrupts
 #
-encoder_rear_left.irq(handler=left_rear_encoder_counter, trigger=Pin.IRQ_RISING)
+# - Front
+# encoder_front_left.irq(handler=front_left_encoder_counter, trigger=Pin.IRQ_RISING)
+encoder_front_right.irq(handler=front_right_encoder_counter, trigger=Pin.IRQ_RISING)
+# - Rear
+encoder_rear_left.irq(handler=rear_left_encoder_counter, trigger=Pin.IRQ_RISING)
+# encoder_rear_right.irq(handler=rear_right_encoder_counter, trigger=Pin.IRQ_RISING)
 #
 # Configure timer interrupt event
 #
@@ -298,7 +373,7 @@ timer.init(freq=1, mode=Timer.PERIODIC, callback=sonic_sense)
 try:
     # start main loop
     main()
-    timer.deinit()
+    #timer.deinit()
     
 except KeyboardInterrupt:
     # Abort, stop bot
@@ -307,5 +382,5 @@ except KeyboardInterrupt:
     
 finally:
     # Cleanup
-    timer.deinit()
+    # timer.deinit()
     print ('*** END ***')
